be2065f9502ec14467368d35ca6e8ba781e4ed4d
JosueB10 <josuebeteta8@gmail.com>
2022-09-26 14:21:50 -0600

    Ejercicio 14 y 15

---
+++ bateriadeejercicios/src/Pagina2/claveTuani15.java
@@ -0,0 +1,30 @@
+package Pagina2;
+import java.util.Scanner;
+public class claveTuani15 {
+
+	public static void main(String[] args) {
+		int i=0;
+		String nom;
+		
+		Scanner SC= new Scanner(System.in);
+		
+				while (i<3)
+				System.out.println("Ingrese su nombre de usuario: ");
+				     nom=SC.nextString();
+				            clave=input("ingrese su clave")
+				            if str(clave)=="SENATI":
+				                  print("Bienvenido Andy")
+				                  break
+				            else:
+				                  print("CLAVE INCORRECTA")
+				                  if    i==3:
+				                        print("INTENTOS AGOTADOS")
+				                        break
+				      else:
+				            print("USUARIO INCORRECTO")
+				            if    i==3:
+				                  print("INTENTOS AGOTADOS")
+
+	}
+
+}
